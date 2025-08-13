e09a0ec06c84bb93f4d7655266da85f71c7e9fa8
Amitabh Verma <averma@mbl.edu>
2025-08-13 14:22:14 -0500

    update

--- waveorder/stokes.py
+++ waveorder/stokes.py
@@ -149,10 +149,10 @@ def stokes_after_adr(
     orientation, transmittance, and depolarization.
 
     Note: all four parameters can be torch.Tensor, but they must be the same size.
-    If your parameters are in a tensor with shape = (4, ...), use 
+    If your parameters are in a tensor with shape = (4, ...), use
     the * operator to expand over the first dimension.
 
-    e.g. stokes_after_adr(*array) is identical to 
+    e.g. stokes_after_adr(*array) is identical to
     stokes_after_adr(array[0], array[1], array[2], array[3]).
 
     Parameters
